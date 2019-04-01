4179e02b6ed5fdb2a2ee26459d3826baf94c318d
Levtrix <m.k.a.janssen@gmail.com>
2019-04-01 12:28:17 +0200

    Forgot to add 2 classes

---
+++ src/main/java/BigIdeaDAL/specification/TicketSpecification.java
@@ -0,0 +1,22 @@
+package BigIdeaDAL.specification;
+
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+
+public class TicketSpecification {
+    private TicketSpecification() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Specifiable getByName(String name) {
+        if (name == null)
+            throw new IllegalArgumentException();
+
+        return new AbstractSpecification() {
+            @Override
+            public Criterion toCriterion() {
+                return Restrictions.eq("name", name.toLowerCase());
+            }
+        };
+    }
+}
